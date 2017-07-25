d09cbceb00c2843868b7e8f54f5bcca761eba005
droidhorde <30442998+droidhorde@users.noreply.github.com>
2017-07-25 19:53:57 +0200

    -- Hints

--- app/src/main/java/com/droidhorde/exmple/helloworld/MainActivity.java
+++ app/src/main/java/com/droidhorde/exmple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package com.droidhorde.exmple.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * This is the entry point to your application for now.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
